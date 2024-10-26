c546461dae067f2735c36379b00348850bedd7ca
TyCanSing-ctrl <ivan3582838@gmail.com>
2024-10-26 08:08:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
